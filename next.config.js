123104763fc12d7b6a968c1278d61c50d922038d
Hasan Islam <kinghasanbd1@gmail.com>
2025-11-15 12:52:24 +0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -20,10 +20,22 @@ const nextConfig = {
             key: 'Access-Control-Allow-Headers',
             value: 'Content-Type, Authorization'
           }
-        ],
-      },
+        ]
+      }
     ]
   },
+  // Remove any problematic options
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  }
 }
 
 module.exports = nextConfig
